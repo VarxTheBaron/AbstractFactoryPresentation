76aa2c1856cb8ea6de1d401c800674155127b5c4
biggieliten <filipisaksson00@gmail.com>
2026-02-11 13:44:28 +0100

    -

--- Program.cs
+++ Program.cs
@@ -1,35 +1,35 @@
 ﻿internal class Program
 {
-    private static void Main(string[] args)
-    {
-        Console.WriteLine("Select the type of car to create:");
-        Console.WriteLine("1. gasoline");
-        Console.WriteLine("2. diesel");
-        Console.WriteLine("3. electrical");
+	private static void Main(string[] args)
+	{
+		Console.WriteLine("Select the type of car to create:");
+		Console.WriteLine("1. Gasoline");
+		Console.WriteLine("2. Diesel");
+		Console.WriteLine("3. Electrical");
 
-        string input = Console.ReadLine() ?? "";
+		string input = Console.ReadLine() ?? "";
 
 
-        RunCar(IVehicleFactoryResolver.GetVehicleFactory(input));
-    }
+		RunCar(IVehicleFactoryResolver.GetVehicleFactory(input.ToLower().Trim()));
+	}
 
-    public static void RunCar(IVehicleFactory factory)
-    {
-        Car car = new Car(factory);
-        car.ShowInfo();
-    }
+	public static void RunCar(IVehicleFactory factory)
+	{
+		Car car = new Car(factory);
+		car.ShowInfo();
+	}
 
-    public class Car(IVehicleFactory factory)
-    {
-        IEngine engine = factory.CreateEngine();
-        IInterior interior = factory.CreateInterior();
-        IWheel wheel = factory.CreateWheel();
+	public class Car(IVehicleFactory factory)
+	{
+		IEngine engine = factory.CreateEngine();
+		IInterior interior = factory.CreateInterior();
+		IWheel wheel = factory.CreateWheel();
 
-        public void ShowInfo()
-        {
-            Console.WriteLine($"Engine: {engine.GetSpecs()}");
-            Console.WriteLine($"Interior: {interior.GetStyle()}");
-            Console.WriteLine($"Wheel: {wheel.GetWheelType()}");
-        }
-    }
+		public void ShowInfo()
+		{
+			Console.WriteLine($"Engine: {engine.GetSpecs()}");
+			Console.WriteLine($"Interior: {interior.GetStyle()}");
+			Console.WriteLine($"Wheel: {wheel.GetWheelType()}");
+		}
+	}
 }
