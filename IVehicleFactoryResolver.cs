0b9374e9a751cd8551b18f691d23b9570cff8c97
biggieliten <filipisaksson00@gmail.com>
2026-02-11 13:36:11 +0100

    add factories to the dict

--- IVehicleFactoryResolver.cs
+++ IVehicleFactoryResolver.cs
@@ -1,19 +1,26 @@
 public static class IVehicleFactoryResolver
 {
-    static private Dictionary<string, IVehicleFactory> factories = new();
+	static private Dictionary<string, IVehicleFactory> factories = new();
 
-    static public IVehicleFactory GetVehicleFactory(string factoryKey)
-    {
-        foreach (var factory in factories)
-        {
-            if (factory.Key == factoryKey) return factory.Value;
-        }
+	static IVehicleFactoryResolver()
+	{
+		AddFactory("car", new DieselCarFactory());
+		AddFactory("truck", new GasolineCarFactory());
+		AddFactory("electric", new ElectricalCarFactory());
+	}
 
-        throw new InvalidDataException();
-    }
+	static public IVehicleFactory GetVehicleFactory(string factoryKey)
+	{
+		foreach (var factory in factories)
+		{
+			if (factory.Key == factoryKey) return factory.Value;
+		}
 
-    static public void AddFactory(string key, IVehicleFactory vehicleFactory)
-    {
-        factories.Add(key, vehicleFactory);
-    }
+		throw new InvalidDataException();
+	}
+
+	static public void AddFactory(string key, IVehicleFactory vehicleFactory)
+	{
+		factories.Add(key, vehicleFactory);
+	}
 }
